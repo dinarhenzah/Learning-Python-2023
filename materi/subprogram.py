1725eae79c759d68784c4dd9139eea723ceb2399
Dinar10 <dinarchter1025@gmail.com>
2024-01-24 05:45:12 +0700

    add materi subprogram

---
+++ materi/subprogram.py
@@ -0,0 +1,19 @@
+# SUBPROGRAM
+
+# 1. Cara umum
+panjang = 10
+lebar = 5
+
+luas = panjang * lebar
+print(luas)
+
+print("==========")
+
+# 2. Cara simple
+def luass(panjangg, lebarr):
+    return panjangg*lebarr
+
+pp = luass(10,5)
+
+print(pp)
+
